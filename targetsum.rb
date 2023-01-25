d20a0a2227945cbb945a0d6629cb9b8aad0c0bf9
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-01-25 13:02:37 +0300

    Added

--- targetsum.rb
+++ targetsum.rb
@@ -1,32 +1,37 @@
 def target_sum(array, target)
-    
-    # Big O: O(n^2)
+    hash = {}
     array.each_with_index do |num, idx|
-        
-        # puts "num: #{num} idx: #{idx}"
-        array.each_with_index do |num2, idx2|
-            # puts "num: #{num} idx: #{idx} num2: #{num2} idx2: #{idx2}"
-         
-            if num + num2 == target && idx != idx2
-                # puts "num: #{num} idx: #{idx} num2: #{num2} idx2: #{idx2}"
-                return [idx, idx2 ]
-                
-            end
-
-            # if you want the duplicate only
-            # if num + num2 == target && idx == idx2
-              
-            #     return [idx]
-                
-            # end
-
+        if hash[target-num] != nil
+            return [idx, hash[target-num]]
         end
+        hash[num] = idx
     end
-    
-    
-
-
-
+    return nil
 end
 
+
+# def target_sum(array, target)
+    
+#     # Big O: O(n^2)
+#     array.each_with_index do |num, idx|
+        
+#         # puts "num: #{num} idx: #{idx}"
+#         array.each_with_index do |num2, idx2|
+#             # puts "num: #{num} idx: #{idx} num2: #{num2} idx2: #{idx2}"
+         
+#             if num + num2 == target && idx != idx2
+#                 # puts "num: #{num} idx: #{idx} num2: #{num2} idx2: #{idx2}"
+#                 return [idx, idx2 ]
+                
+#             end
+
+#             # if you want the duplicate only
+#             # if num + num2 == target && idx == idx2
+              
+#             #     return [idx]
+                
+#             # end
+
+#         end
+#     end
 puts target_sum([1, 2, 3, 4, 5], 8) 
